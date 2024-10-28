349c798fb84df4511eab831ade1c9ab55bd6fc5f
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-28 17:10:48 +0000

    Add functions to check for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,38 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const splitString = string.split("");
+    const word = returnString(splitString);
+    const reversedWord = returnReversedString(splitString);
 
+    if (word === reversedWord) {
+        return true;
+    }
+    return false;
 };
+
+function isLetter(char) {
+    return /^[a-zA-Z0-9]$/.test(char);
+}
+
+function returnString(string) {
+    const newString = [];
+    for (i = 0; i <= string.length; i++) {
+        if (isLetter(string[i])) {
+            newString.push(string[i]);
+        }
+    }
+    return newString.join("").toLowerCase();
+}
+
+function returnReversedString(string) {
+    const newString = [];
+    for (i = string.length; i >= 0; i--) {
+        if (isLetter(string[i])) {
+            newString.push(string[i]);
+        }
+    }
+    return newString.join("").toLowerCase();
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
