75b8bae6e38afd861307c283863b93ed76f207d0
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-28 18:51:32 +0000

    Add fibonacci sequence
    Add function that calculates fibonacci sequence and
    returns specific number in a sequence.
    All tests passed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(fibonacciNumber) {
+    let fibonacciArray = [];
+    if (fibonacciNumber < 0) {
+        return "OOPS"
+    }
+      for (i = 0; i <= fibonacciNumber; i++) {
+        if (i === 0) {
+          fibonacciArray.push(0);
+        } else if (i === 1) {
+          fibonacciArray.push(1);
+        } else {
+          fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+        }
+      }
+      return fibonacciArray[fibonacciNumber];
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
