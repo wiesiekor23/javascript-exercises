9adcecd31cdf4603279d0ec6197f1c109784fc8d
Daniel Niechaj <181674388+wiesiekor23@users.noreply.github.com>
2024-10-28 16:11:36 +0000

    Fill the functions with code to pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, value) => sum + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multi, value) => multi * value, 1);
 };
 
-const power = function() {
-	
+
+const power = function(number, power) {
+  const array = Array(power).fill(number, 0);
+  return multiply(array);
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  const array = [];
+  for (i = factorial; i > 0; i--) {
+    array.push(i);
+  }
+  return multiply(array);
 };
 
 // Do not edit below this line
